e76bb0503529f3d3d54d7f54e70f50b2bc070cf2
MitkaS7 <mitka.dev@gmail.com>
2023-03-25 17:39:40 +0300

    dev: refact img gulp task

--- gulpTasks/img.js
+++ gulpTasks/img.js
@@ -1,26 +1,16 @@
-// обработка pug
-const { src, dest } = require('gulp');
-
-// конфиг
-const path = require('../config/path.js');
-const app = require('../config/app.js');
-//плагины
-const plumber = require('gulp-plumber');
-const notify = require('gulp-notify');
-const imageMin = require('gulp-imagemin');
-const gulpNewer = require('gulp-newer');
-const webp = require('gulp-webp');
+// обработка images
 
 const img = () => {
-  return src(path.img.src)
-    .pipe(plumber())
-    .pipe(gulpNewer(path.img.dest))
-    .pipe(webp())
-    .pipe(dest(path.img.dest))
-    .pipe(gulpNewer(path.img.dest))
-    .pipe(imageMin(app.imageMin))
-    .pipe(src(path.img.src))
-    .pipe(dest(path.img.dest))
+  return $.gulp.src($.path.img.src)
+    .pipe($.plugins.plumber())
+    .pipe($.plugins.newer($.path.img.dest))
+    .pipe($.plugins.webp())
+    .pipe($.gulp.dest($.path.img.dest))
+    .pipe($.plugins.newer($.path.img.dest))
+    .pipe($.plugins.imagemin($.app.imageMin))
+    .pipe($.gulp.src($.path.img.src))
+    .pipe($.gulp.dest($.path.img.dest))
+    .pipe($.browserSync.stream());
 
 }
 module.exports = img;
