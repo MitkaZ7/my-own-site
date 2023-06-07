5dedd9b5f8b89a5536f40e9618da201503644098
Дмитрий Западнов <mitkamac@MacBook-Pro-Mitka.local>
2023-06-07 11:41:30 +0300

    dev: add slide block

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,24 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger.js";
-import { ScrollSmoother } from 'gsap/ScrollSmoother.js';
+import gsap from "gsap-trial";
+import { ScrollTrigger } from "gsap-trial/ScrollTrigger.js";
+import { ScrollSmoother } from "gsap-trial/ScrollSmoother.js";
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-let scrollPosition = 0;
-window.addEventListener('scroll', (evt) => {
-  scrollPosition = window.scrollY;
-  // console.log(scrollPosition);
-  document.body.style.cssText = `--scrollTop: ${scrollPosition}px`
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
+ScrollSmoother.create({
+  wrapper: '.wrapper',
+  content: '.content',
+  smooth: 1.4,
+  effects: true,
 })
+
+
+
+
+let scrollPosition = 0;
+// window.addEventListener('scroll', (evt) => {
+//   scrollPosition = window.scrollY;
+//   // console.log(scrollPosition);
+//   document.body.style.cssText = `--scrollTop: ${scrollPosition}px`
+// })
 
 
 const quoteElement = document.querySelector('.main_article__header');
