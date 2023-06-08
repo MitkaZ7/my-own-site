dc8e4bfa5a3dfc5d4c594830a0fe2cb6c4a4199a
Дмитрий Западнов <mitkamac@MacBook-Pro-Mitka.local>
2023-06-08 14:04:33 +0300

    before migration to one section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,78 @@
 import gsap from "gsap-trial";
 import { ScrollTrigger } from "gsap-trial/ScrollTrigger.js";
 import { ScrollSmoother } from "gsap-trial/ScrollSmoother.js";
-
 gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
-ScrollSmoother.create({
-  wrapper: '.wrapper',
-  content: '.content',
-  smooth: 1.4,
-  effects: true,
-})
 
+// const tl = gsap.timeline();
+// tl.from('.hedaer', {
+//     xPercent: -100,
+//   })
+//   .from('.projects-section',{
+//     xPercent: 100,
+//   })
+//   .from('.stack-section', {
+//     xPercent: -100,
+//   })
+
+// gsap.utils.toArray('.section').forEach((section, i) => {
+//   ScrollTrigger.create({
+//     animation: tl,
+//     trigger: section,
+//     start: 'top top',
+//     pin: true,
+//     pinSpacing: false,
+//   })
+// })
+
+
+
+
+// анимация заголовка рабочая--------
+// let tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: '.header__titles',
+//     start: 'center 80%',
+//     end: 'bottom 20%',
+//     toggleActions: "play reverse play reverse",
+//     markers: true,
+//     scrub: true,
+//   },
+// })
+//---------
+
+// let animation = anime({
+//   targets: '.header__titles',
+//   translateY: 300,
+//   opacity: 0,
+//   easing: 'easeInOutSine',
+//   autoplay: false
+// });
+
+// window.onscroll = (e) => {
+//   animation.seek(window.pageYOffset);
+//   console.log(window.pageXOffset)
+// }
+
+
+// ScrollSmoother.create({
+//   wrapper: '.wrapper',
+//   content: '.main',
+//   smooth: 1.4,
+//   effects: true,
+// })
+// tl.to('.header__titles', {
+//   x: 1100,
+//   rotation: 90,
+//   duration: 4,
+//   ease: 'none'
+// })
+tl.from('.header__titles', {
+    opacity: 1, duration: 1,
+},1)
+tl.to('.header__titles', {
+  opacity: 0,
+  duration: 1
+})
 
 
 
@@ -20,17 +83,3 @@ let scrollPosition = 0;
 //   document.body.style.cssText = `--scrollTop: ${scrollPosition}px`
 // })
 
-
-const quoteElement = document.querySelector('.main_article__header');
-// function getQuote() {
-//     fetch('https://api.kanye.rest')
-//     .then(res => res.json())
-//     .then((data) => {
-//       console.log(data)
-//         quoteElement.textContent = data.quote;
-
-//     });
-// }
-
-// const btn = document.querySelector('.button');
-// btn.addEventListener('click', getQuote())
